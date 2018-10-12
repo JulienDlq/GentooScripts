1a77b477097113509a67227f95a28079de4ef941
JulienDlq <julien.dlq@gmail.com>
2018-10-12 09:10:14 +0200

    Amélioration : [Grub] Vérification du nombre de noyaux présents dans /boot
    Maintenant, le script est capable de vérifier s'il y a trop de noyaux dans /boot
    et prévenir l'utilisateur si la limite est dépassée

--- gentoo-grub.sh
+++ gentoo-grub.sh
@@ -7,8 +7,24 @@ cd $SCRIPTPATH
 # Initialisation du script
 . ./init
 
+# Monter la partition de boot
 mount /boot
+
+# Vérification du nombre de noyaux présents
+NBNOYAUX=$(cd /boot ; ls -1 *gentoo | sed 's/.*-x86_64-//' | sort | uniq | wc -l)
+echo "Il y a actuellement" $NBNOYAUX "noyaux dans /boot"
+
+if [[ $NBNOYAUX -gt 2 ]]
+then
+	echo "Il serait temps de faire du ménage !"
+fi
+
+echo
+
+# Reconfiguration de grub
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Démonter la partition de boot
 umount /boot
 
 exit 0
