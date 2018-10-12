e9fa3ff75b6d1978b03a79cdeab9f6c52f4c8a96
JulienDlq <julien.dlq@gmail.com>
2018-10-12 14:39:55 +0200

    Amélioration : [Grub] Suppressions des noyaux obsolètes présents dans /boot
    Maintenant, le script est capable de supprimer les noyaux obsolètes dans /boot
    afin de gagner un peu de place sur la partition.

--- gentoo-grub.sh
+++ gentoo-grub.sh
@@ -7,18 +7,56 @@ cd $SCRIPTPATH
 # Initialisation du script
 . ./init
 
+# Configuration du script
+NBMAXKERNEL=2
+
 # Monter la partition de boot
 mount /boot
 
-# Vérification du nombre de noyaux présents
-NBNOYAUX=$(cd /boot ; ls -1 *gentoo | sed 's/.*-x86_64-//' | sort | uniq | wc -l)
-echo "Il y a actuellement" $NBNOYAUX "noyaux dans /boot"
-
-if [[ $NBNOYAUX -gt 2 ]]
+# Affichage du nombre de noyaux présents
+KernelVersions=($(ls -1 /boot/*gentoo | sed 's/.*-x86_64-//' | sort -V | uniq))
+echo -n "Il y a "${#KernelVersions[*]}" Noyau(x) dans /boot => "
+if [[ ${#KernelVersions[*]} -gt $NBMAXKERNEL ]]
 then
 	echo "Il serait temps de faire du ménage !"
+elif [[ ${#KernelVersions[*]} -eq $NBMAXKERNEL ]]
+then
+	echo "Ok."
+else
+	echo "Il y a moins de noyau(x) que le max prévu ("$NBMAXKERNEL")."
+	NBMAXKERNEL=${#KernelVersions[*]}
 fi
+echo
 
+# Affichage de ces noyaux
+echo "Liste de(s) noyau(x) :"
+for Kernel in ${KernelVersions[*]}
+do
+    echo "- "${Kernel}
+done
+echo
+
+# Affichage des noyaux à conserver
+echo "Il faudrait conserver le(s) noyau(x) suivant(s) :"
+for (( Kernel=1 ; Kernel <= $NBMAXKERNEL ; Kernel++ ))
+do
+	echo "- "${KernelVersions[-1]}
+	unset KernelVersions[-1]
+done
+echo
+
+# Suppression des versions obsolètes
+if [[ ${#KernelVersions[*]} -eq 0 ]]
+then
+	echo "Pas de noyau à supprimer."
+else
+	echo "Le(s) noyau(x) suivant(s) sont supprimés :"
+fi
+for Kernel in ${KernelVersions[*]}
+do
+    echo "- "${Kernel}
+	rm /boot/*-x86_64-${Kernel}
+done
 echo
 
 # Reconfiguration de grub
