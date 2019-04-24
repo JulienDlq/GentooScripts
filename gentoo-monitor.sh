bbf95fbe4096d1fbf828b6f2437a2cb63698c24b
JulienDlq <julien.dlq@gmail.com>
2019-04-24 18:07:09 +0200

    Amélioration : [Monitor] Ajout des températures
    Il est maintenant possible de monitorer la température d'une Gentoo pendant sa mise à jour.

--- gentoo-monitor.sh
+++ gentoo-monitor.sh
@@ -14,7 +14,7 @@ iotop_py=$(( $top_py + $top_sy + 50 ))                        # Linked to top wi
 iotop_sx=$(( $top_sx ))                                       # Linked to top window
 iotop_sy=$(( 99 * $coef - $iotop_py ))                        # Reference
 
-ccache_sx=$(( 650 ))                                          # Reference
+ccache_sx=$(( 480 ))                                          # Reference
 ccache_sy=$(( 485 ))                                          # Reference
 ccache_px=$(( $top_px + $top_sx + 25 ))                       # Linked to top window
 ccache_py=$(( $top_py ))                                      # Linked to top window
@@ -29,6 +29,11 @@ genlop_sy=$(( 200 ))                                          # Reference
 genlop_px=$(( $ccache_root_px + $ccache_root_sx + 25 ))       # Linked to ccache_root window
 genlop_py=$(( $ccache_root_py + 5 * $coef ))                  # Linked to ccache_root window
 
+sensors_sx=$(( 540 ))                                         # Reference
+sensors_sy=$(( 290 ))                                         # Reference
+sensors_px=$(( $ccache_px + $ccache_sx + 25 ))                # Linked to ccache window
+sensors_py=$(( $ccache_py ))                                  # Linked to ccache window
+
 # Il faut ajouter les lignes suivantes dans le fichier /etc/sudoers afin de ne pas avoir à fournir le mot de passe à sudo
 # %wheel ALL=(ALL) NOPASSWD: /usr/bin/watch -n1 -- ccache -s
 # %wheel ALL=(ALL) NOPASSWD: /usr/bin/iotop -o
@@ -40,6 +45,7 @@ konsole -qwindowgeometry ${iotop_sx}x${iotop_sy}+${iotop_px}+${iotop_py} -e 'sud
 konsole -qwindowgeometry ${ccache_sx}x${ccache_sy}+${ccache_px}+${ccache_py} -e 'watch -n1 -- CCACHE_DIR=/var/tmp/ccache/ ccache -s' &
 konsole -qwindowgeometry ${ccache_root_sx}x${ccache_root_sy}+${ccache_root_px}+${ccache_root_py} -e 'sudo watch -n1 -- ccache -s' &
 konsole -qwindowgeometry ${genlop_sx}x${genlop_sy}+${genlop_px}+${genlop_py} -e 'sudo watch -n1 -c -- genlop -c' &
+konsole -qwindowgeometry ${sensors_sx}x${sensors_sy}+${sensors_px}+${sensors_py} -e 'watch -n1 -c -- sensors' &
 
 exit 0
 
