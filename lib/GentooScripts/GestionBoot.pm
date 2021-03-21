3b099355423dd8e76f2cc717e1e2c6e2dd66646c
JulienDlq <julien.dlq@gmail.com>
2021-03-21 21:36:00 +0100

    Correction: [Core] PerlTidy

--- lib/GentooScripts/GestionBoot.pm
+++ lib/GentooScripts/GestionBoot.pm
@@ -19,41 +19,47 @@ our $BOOT = '/boot';
 # Monter la partition de boot
 sub monter_boot {
 
-    # Vérification pour éviter d'avoir des messages d'erreur prévisible
-    # de la commande mount
-    my $sortie_mount =
-      executer( 'mount | grep ' . $BOOT . ' 2>/dev/null 1>&2' );
+	# Vérification pour éviter d'avoir des messages d'erreur prévisible
+	# de la commande mount
+	my $sortie_mount =
+	  executer( 'mount | grep ' . $BOOT . ' 2>/dev/null 1>&2' );
 
-    if ( $sortie_mount == 1 ) {
-        journaliser( 'La partition ' . $BOOT . ' va être montée.' );
-        system 'mount ' . $BOOT;
-    }
-    elsif ( $sortie_mount == 0 ) {
-        journaliser( 'La partition ' . $BOOT . ' est déjà montée.' );
-    }
-    else {
-        journaliser( 'Erreur non gérée (Montage "' . $BOOT . '").' );
-    }
+	if ( $sortie_mount == 1 ) {
+
+		journaliser( 'La partition ' . $BOOT . ' va être montée.' );
+		system 'mount ' . $BOOT;
+
+	} elsif ( $sortie_mount == 0 ) {
+
+		journaliser( 'La partition ' . $BOOT . ' est déjà montée.' );
+
+	} else {
+
+		journaliser( 'Erreur non gérée (Montage "' . $BOOT . '").' );
+	}
 }
 
 # Démonter la partition de boot
 sub demonter_boot {
 
-    # Vérification pour éviter d'avoir des messages d'erreur prévisible
-    # de la commande umount
-    my $sortie_mount =
-      executer( 'mount | grep ' . $BOOT . ' 2>/dev/null 1>&2' );
+	# Vérification pour éviter d'avoir des messages d'erreur prévisible
+	# de la commande umount
+	my $sortie_mount =
+	  executer( 'mount | grep ' . $BOOT . ' 2>/dev/null 1>&2' );
 
-    if ( $sortie_mount == 0 ) {
-        journaliser( 'La partition ' . $BOOT . ' va être démontée.' );
-        system 'umount ' . $BOOT;
-    }
-    elsif ( $sortie_mount == 1 ) {
-        journaliser( 'La partition ' . $BOOT . ' est déjà démontée.' );
-    }
-    else {
-        journaliser( 'Erreur non gérée (Démontage "' . $BOOT . '").' );
-    }
+	if ( $sortie_mount == 0 ) {
+
+		journaliser( 'La partition ' . $BOOT . ' va être démontée.' );
+		system 'umount ' . $BOOT;
+
+	} elsif ( $sortie_mount == 1 ) {
+
+		journaliser( 'La partition ' . $BOOT . ' est déjà démontée.' );
+
+	} else {
+
+		journaliser( 'Erreur non gérée (Démontage "' . $BOOT . '").' );
+	}
 }
 
 1;
