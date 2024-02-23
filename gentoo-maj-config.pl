356935cce9b02977064a6896c142a075ec2c262a
JulienDlq <julien.dlq@gmail.com>
2024-02-23 23:47:37 +0100

    Correction: [Mise à jour] La configuration était fausse
    Le mode « quiet » était à 0 au lieu de 1.

--- gentoo-maj-config.pl
+++ gentoo-maj-config.pl
@@ -4,7 +4,7 @@ use warnings;
 # Configuration du script
 our $CONFIG = {
 	'verbose' => 0,    # Pas de verbose (0)
-	'quiet'   => 0,    # Pas de messages de compilation sur la sortie standard (0)
+	'quiet'   => 1,    # Pas de messages de compilation sur la sortie standard (1)
 	'force'   => 0,    # Pas de force de la mise à jour (0)
 };
 
